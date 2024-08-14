d344cc53dc947f2fc5cdb7b0c5fe4bf35eb2e695
ImBatmanCyBall <107407894+ImBatmanCyBall@users.noreply.github.com>
2024-08-14 20:42:04 +0530

    Updated data caching rules

--- CrashTest/ServiceWorker.js
+++ CrashTest/ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "XEX-XEX-Game-0.1.9.10C";
-const contentToCache = [
-    "Build/CrashTest.loader.js",
-    "Build/CrashTest.framework.js",
-    "Build/CrashTest.data",
-    "Build/CrashTest.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
