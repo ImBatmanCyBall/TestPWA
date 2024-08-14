23fae245061f9c83d99f3c2f79624be01cfe7401
ImBatmanCyBall <107407894+ImBatmanCyBall@users.noreply.github.com>
2024-08-14 21:39:32 +0530

    Test3

---
+++ CrashTest3/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "XEX-XEX-Game-0.1.9.10C";
+const contentToCache = [
+    "Build/CrashTest3.loader.js",
+    "Build/CrashTest3.framework.js",
+    "Build/CrashTest3.data",
+    "Build/CrashTest3.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
